813ea15640f460afb312754743fd945e257d49d2
goingif <954735429@qq.com>
2019-04-07 10:18:41 +0800

    新增静态页控制器和方法

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+@section('content')
+@section('title','关于')
+<h1>关于页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.default')
+@section('title','主页')
+@section('content')
+<h1>主页</h1>
